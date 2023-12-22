4ea1451d0e860dd6fbc61fb577cdec3fcf63b5f4
Denis Pavlovich <4.fun.denis@gmail.com>
2023-12-22 13:01:45 +0700

    fix code

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -5,10 +5,12 @@ import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         UserService userService = context.getBean(UserService.class);
@@ -16,7 +18,7 @@ public class MainApp {
         userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
         userService.add(new User("User2", "Lastname2", "user2@mail.ru").setNoCar(new Car("Velik", 1)));
         userService.add(new User("User3", "Lastname3", "user3@mail.ru").setNoCar(new Car("LADA", 6)));
-        userService.add(new User("User4", "Lastname4", "user4@mail.ru").setNoCar(new Car("BMW", 16)));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru").setNoCar(new Car("BMW", 5)));
 
 
         List<User> users = userService.listUsers();
